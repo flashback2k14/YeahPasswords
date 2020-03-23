c0d6993fb6ba21d41b2fef3cea3a1cef480b5b89
flashback2k14 <yeahdev@gmail.com>
2020-03-23 19:55:00 +0100

    dev: simplify naming

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:yeah_passwords/src/yeah_password_app.dart';
+import 'package:yeah_passwords/src/app.dart';
 
-void main() => runApp(YeahPasswordApp());
+void main() => runApp(App());
 
 // https://medium.com/@azamsharp/understanding-global-state-in-flutter-using-redux-2017b7646574
 // https://flutter.dev/docs/development/ui/widgets/material
