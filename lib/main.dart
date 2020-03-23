f02ee3ea83d2f28ff6255e7ebafb591454cf31b7
flashback2k14 <yeahdev@gmail.com>
2020-03-23 21:18:48 +0100

    dev: add database + repository

--- lib/main.dart
+++ lib/main.dart
@@ -6,3 +6,7 @@ void main() => runApp(App());
 // https://medium.com/@azamsharp/understanding-global-state-in-flutter-using-redux-2017b7646574
 // https://flutter.dev/docs/development/ui/widgets/material
 // https://pusher.com/tutorials/login-ui-flutter
+
+// https://flutter.dev/docs/cookbook/persistence/sqlite
+// https://pusher.com/tutorials/local-data-flutter
+// https://pub.dev/packages/crypto
