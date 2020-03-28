df06c63416422106d2a315d51f6f2f1220942ccc
flashback2k14 <yeahdev@gmail.com>
2020-03-28 22:00:57 +0100

    dev: clear inputs, add appbar action

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,9 @@ import 'package:yeah_passwords/src/app.dart';
 
 void main() => runApp(App());
 
+// https://bloclibrary.dev/#/recipesfluttershowsnackbar
 // https://medium.com/@azamsharp/understanding-global-state-in-flutter-using-redux-2017b7646574
 // https://flutter.dev/docs/development/ui/widgets/material
-// https://pusher.com/tutorials/login-ui-flutter
 
 // https://flutter.dev/docs/cookbook/persistence/sqlite
 // https://pusher.com/tutorials/local-data-flutter
-// https://pub.dev/packages/crypto
