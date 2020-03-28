c5cbdfc571f241c209857533eadde39596f9465f
flashback2k14 <yeahdev@gmail.com>
2020-03-28 20:47:47 +0100

    add sign up page, add input widget

--- lib/src/pages/signup_page.dart
+++ lib/src/pages/signup_page.dart
@@ -1,64 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:yeah_passwords/src/widgets/yeah_input.dart';
 
-class LoginPage extends StatefulWidget {
-  LoginPage({Key key, this.title}) : super(key: key);
+class SignupPage extends StatefulWidget {
+  SignupPage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _LoginPageState createState() => _LoginPageState();
+  _SignupPageState createState() => _SignupPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _SignupPageState extends State<SignupPage> {
   YeahInput usernameInput = YeahInput(labelText: "Username", isPassword: false);
   YeahInput passwordInput = YeahInput(labelText: "Password", isPassword: true);
+  YeahInput confirmPasswordInput =
+      YeahInput(labelText: "Confirm Password", isPassword: true);
 
-  void _performLogin(BuildContext context) async {
+  void _performSignUp(BuildContext context) async {
     // var user = User(name: 'test', passwordHash: 'sldjfdhfhudf');
     // await UserRepository().insert(user);
     // List<User> users = await UserRepository().findAll();
     // print(users);
 
-    if (usernameInput.getText() == "lorem" &&
-        passwordInput.getText() == "ipsum") {
+    if (passwordInput.getText() == confirmPasswordInput.getText()) {
       Navigator.pushNamed(context, "/home");
     } else {
       showDialog(
         context: context,
         builder: (context) {
           return AlertDialog(
-            content: Text("Wrong credentials."),
+            content: Text("Passwords not matching."),
           );
         },
       );
     }
   }
 
-  void _performNavigation(BuildContext context) {
-    Navigator.pushNamed(context, "/signup");
-  }
-
-  ButtonTheme _buildSignInButtonTheme(BuildContext context, String buttonText) {
+  ButtonTheme _buildSignUpButtonTheme(BuildContext context, String buttonText) {
     return ButtonTheme(
       height: 48,
       minWidth: double.infinity,
       child: RaisedButton(
         child: Text(buttonText),
         textColor: Colors.white,
-        onPressed: () => _performLogin(context),
-      ),
-    );
-  }
-
-  ButtonTheme _buildSignUpButtonTheme(BuildContext context, String buttonText) {
-    return ButtonTheme(
-      height: 48,
-      minWidth: double.infinity,
-      child: FlatButton(
-        child: Text(buttonText),
-        textColor: Colors.green,
-        onPressed: () => _performNavigation(context),
+        onPressed: () => _performSignUp(context),
       ),
     );
   }
@@ -68,6 +53,7 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        automaticallyImplyLeading: false,
       ),
       body: Center(
           child: Container(
@@ -79,11 +65,11 @@ class _LoginPageState extends State<LoginPage> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         usernameInput,
-                        SizedBox(height: 25.0),
+                        SizedBox(height: 24.0),
                         passwordInput,
-                        SizedBox(height: 25.0),
-                        _buildSignInButtonTheme(context, "Sign In"),
-                        SizedBox(height: 25.0),
+                        SizedBox(height: 24.0),
+                        confirmPasswordInput,
+                        SizedBox(height: 6.0),
                         _buildSignUpButtonTheme(context, "Sign Up"),
                       ])))),
     );
---
+++ lib/src/widgets/yeah_input.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class YeahInput extends StatelessWidget {
+  YeahInput({Key key, this.labelText, this.isPassword}) : super(key: key);
+
+  final TextEditingController controller = new TextEditingController();
+  final String labelText;
+  final bool isPassword;
+
+  String getText() {
+    return controller.text.trim();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText: isPassword,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: labelText,
+      ),
+    );
+  }
+}
