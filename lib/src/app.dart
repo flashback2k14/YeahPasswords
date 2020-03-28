05476eac9d213c8127349ff888d55a5fcff13d03
flashback2k14 <yeahdev@gmail.com>
2020-03-28 20:47:47 +0100

    add sign up page, add input widget

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yeah_passwords/src/pages/login_page.dart';
+import 'package:yeah_passwords/src/pages/signup_page.dart';
+import 'package:yeah_passwords/src/pages/home_page.dart';
 
 class App extends StatelessWidget {
   @override
@@ -7,9 +9,18 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Yeah! Passwords',
       theme: ThemeData(primarySwatch: Colors.green),
-      home: LoginPage(
-        title: 'Yeah! Password - Login',
-      ),
+      initialRoute: "/login",
+      routes: <String, WidgetBuilder>{
+        "/login": (context) => LoginPage(
+              title: 'Yeah! Password - Sign In',
+            ),
+        "/signup": (context) => SignupPage(
+              title: 'Yeah! Password - Sign Up',
+            ),
+        "/home": (context) => HomePage(
+              title: 'Yeah! Passwords - Home',
+            )
+      },
     );
   }
 }
