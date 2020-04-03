785982f9b4967d71afc33cebe2ece37d3e7f517f
flashback2k14 <yeahdev@gmail.com>
2020-04-03 22:28:10 +0200

    dev: extend bottom sheet and navigation

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:nfc_in_flutter/nfc_in_flutter.dart';
+import 'package:yeah_passwords/src/widgets/yeah_button.dart';
 import 'package:yeah_passwords/src/widgets/yeah_input.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,11 +17,17 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-  final TextEditingController _textEditingController =
-      new TextEditingController();
+  PersistentBottomSheetController _controller;
+  bool _isBottomsheetVisible;
+
   StreamSubscription<NDEFMessage> _stream;
 
-  List<String> items = [];
+  List<String> _items = [];
+
+  final YeahInput providerName = YeahInput(
+      labelText: "Provider name", isPassword: false, isLastInput: false);
+  final YeahInput providerPassword = YeahInput(
+      labelText: "Provider password", isPassword: true, isLastInput: true);
 
   void _showSnackbar(String message) {
     final snackBar = SnackBar(content: Text(message));
@@ -29,19 +36,19 @@ class _HomePageState extends State<HomePage> {
 
   void _startScanning() {
     setState(() {
-      items.clear();
+      _items.clear();
 
       _stream = NFC
           .readNDEF(alertMessage: "Custom message")
           .listen((NDEFMessage message) {
         if (message.isEmpty) {
-          _showSnackbar("read empty ndef message");
+          _showSnackbar("The card is empty.");
           return;
         }
-        _showSnackbar("ndef message ${message.records.length} records.");
+        _showSnackbar("${message.records.length} records read.");
 
         for (NDEFRecord record in message.records) {
-          items.add(record.data);
+          _items.add(record.data);
           setState(() {});
         }
       });
@@ -57,10 +64,10 @@ class _HomePageState extends State<HomePage> {
 
   void _toggleScan() {
     if (_stream == null) {
-      _showSnackbar("start...");
+      _showSnackbar("start reading...");
       _startScanning();
     } else {
-      _showSnackbar("stop...");
+      _showSnackbar("stop reading...");
       _stopScanning();
     }
   }
@@ -68,7 +75,7 @@ class _HomePageState extends State<HomePage> {
   void _write(BuildContext context) async {
     _stopScanning();
 
-    List<NDEFRecord> records = items.map((record) {
+    List<NDEFRecord> records = _items.map((record) {
       return NDEFRecord.plain(record);
     }).toList();
     NDEFMessage message = NDEFMessage.withRecords(records);
@@ -97,34 +104,66 @@ class _HomePageState extends State<HomePage> {
   }
 
   void _onAddItemPressed() {
-    _scaffoldKey.currentState.showBottomSheet<Null>((BuildContext context) {
+    setState(() {
+      _isBottomsheetVisible = true;
+    });
+
+    _controller =
+        _scaffoldKey.currentState.showBottomSheet((BuildContext context) {
+      final YeahButton submitButton = YeahButton(
+          buttonText: "Add Item", isSecondary: false, onPressed: _onSubmit);
+
       return new Container(
-        decoration: new BoxDecoration(color: Colors.blueGrey),
+        decoration: new BoxDecoration(color: Colors.black12),
+        height: 260,
         child: new Padding(
-          padding: const EdgeInsets.fromLTRB(32.0, 50.0, 32.0, 32.0),
-          child: new TextField(
-            controller: _textEditingController,
-            decoration: InputDecoration(
-              hintText: 'Please enter a task',
-            ),
-            onSubmitted: _onSubmit,
-          ),
-        ),
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  providerName,
+                  SizedBox(height: 12.0),
+                  providerPassword,
+                  SizedBox(height: 12.0),
+                  submitButton
+                ])),
       );
+    });
+
+    _controller.closed.then((value) {
+      setState(() {
+        _isBottomsheetVisible = false;
+      });
     });
   }
 
-  _onSubmit(String s) {
-    if (s.isNotEmpty) {
-      items.add(s);
-      _textEditingController.clear();
+  void _hideBottomSheet() {
+    _controller.close();
+    setState(() {
+      _isBottomsheetVisible = false;
+    });
+  }
+
+  void _onSubmit() {
+    if (providerName.getText().isNotEmpty &&
+        providerPassword.getText().isNotEmpty) {
+      _items.add(providerName.getText() + " - " + providerPassword.getText());
+      providerName.clear();
+      providerPassword.clear();
       setState(() {});
     }
   }
 
-  _onDeleteItemPressed(item) {
-    items.removeAt(item);
+  void _onDeleteItemPressed(item) {
+    _items.removeAt(item);
     setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _isBottomsheetVisible = false;
   }
 
   @override
@@ -177,14 +216,14 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       body: new Container(
-        child: items.length == 0
+        child: _items.length == 0
             ? Center(child: Text("Please scan your Cards."))
             : new ListView.builder(
-                itemCount: items.length,
+                itemCount: _items.length,
                 itemBuilder: (context, index) {
                   return ListTile(
                     title: Text(
-                      '${items[index]}',
+                      '${_items[index]}',
                     ),
                     trailing: new IconButton(
                       icon: new Icon(Icons.delete),
@@ -197,12 +236,15 @@ class _HomePageState extends State<HomePage> {
               ),
       ),
       floatingActionButton: new FloatingActionButton(
-        onPressed: () {
-          _onAddItemPressed();
-        },
+        child:
+            _isBottomsheetVisible ? new Icon(Icons.clear) : new Icon(Icons.add),
         tooltip: 'Add task',
-        child: new Icon(Icons.add),
+        onPressed: () {
+          _isBottomsheetVisible ? _hideBottomSheet() : _onAddItemPressed();
+        },
       ),
     );
   }
 }
+
+// https://medium.com/flutter-community/flutter-beginners-guide-to-using-the-bottom-sheet-b8025573c433
