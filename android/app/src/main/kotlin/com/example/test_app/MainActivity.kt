61f507466dfd4ff9ea0eaa6f767b651c387f84a8
flashback2k14 <yeahdev@gmail.com>
2020-04-06 21:19:30 +0200

    dev: open nfc settings

--- android/app/src/main/kotlin/com/example/test_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/test_app/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.yeahdev.yeah_passwords
 
+import android.content.Intent
+import android.os.Build
+import android.provider.Settings
+import android.nfc.NfcAdapter
 import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+
+    companion object {
+        private const val CHANNEL = "com.yeahdev.yeah_passwords/intent"
+        const val CHECK_NFC_STATE_METHOD = "check_nfc_state"
+        const val TOGGLE_NFC_STATE_METHOD = "toggle_nfc_state"
+    }
+
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            when(call.method) {
+                CHECK_NFC_STATE_METHOD -> {
+                    var nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+                    result.success(nfcAdapter.isEnabled())
+                }
+                TOGGLE_NFC_STATE_METHOD -> {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                        startActivity(Intent(Settings.ACTION_NFC_SETTINGS))
+                    } else {
+                        startActivity(Intent(Settings.ACTION_WIRELESS_SETTINGS))
+                    }
+                    result.success(null)
+                }
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        }
     }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,3 +12,5 @@ void main() => runApp(App());
 
 // https://github.com/guivazcabral/flutter_todo/blob/master/lib/main.dart
 // https://github.com/semlette/nfc_in_flutter/blob/master/example/lib/write_example_screen.dart
+
+// https://medium.com/flutter-community/flutter-beginners-guide-to-using-the-bottom-sheet-b8025573c433
