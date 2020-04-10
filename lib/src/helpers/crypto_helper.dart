147a5b31b447cc90a858c731248fdb098a581a98
flashback2k14 <yeahdev@gmail.com>
2020-04-10 13:01:43 +0200

    dev: add crypted items

---
+++ lib/src/helpers/crypto_helper.dart
@@ -0,0 +1,26 @@
+import 'dart:typed_data';
+
+import 'package:encrypt/encrypt.dart';
+
+class CryptoHelper {
+  factory CryptoHelper() => _instance;
+  static final CryptoHelper _instance = CryptoHelper._internal();
+
+  Encrypter _encrypter;
+  final _iv = IV.fromLength(8);
+
+  CryptoHelper._internal() {
+    final key = Key.fromUtf8(
+        "kdfjdfahgdfje4rkewfmdfnj!?egn434g4389g7dndkfjhg4r389rgerg");
+    this._encrypter = Encrypter(Salsa20(key));
+  }
+
+  Uint8List encrypt(List<int> plainTextBytes) {
+    return this._encrypter.encryptBytes(plainTextBytes, iv: this._iv).bytes;
+  }
+
+  String decrypt(Uint8List encryptedText) {
+    Encrypted encrypted = new Encrypted(encryptedText);
+    return this._encrypter.decrypt(encrypted, iv: this._iv);
+  }
+}
--- lib/src/models/provider-item_model.dart
+++ lib/src/models/provider-item_model.dart
@@ -1,13 +1,40 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:yeah_passwords/src/helpers/crypto_helper.dart';
+
 class ProviderItem {
   String name;
   String password;
 
   ProviderItem({this.name, this.password});
 
-  ProviderItem.fromString(String savedValue) {
+  ProviderItem.createEmptyItem() {
+    name = "EMPTY";
+    password = "EMPTY";
+  }
+
+  ProviderItem.fromUint8List(Uint8List payload) {
+    this.fromString(CryptoHelper().decrypt(payload));
+  }
+
+  void fromString(String savedValue) {
     List<String> parts = savedValue.split('#VP#');
-    name = parts[0].substring(2);
-    password = parts[1];
+    if (parts.length < 2) {
+      return;
+    }
+    this.name = parts[0];
+    this.password = parts[1];
+  }
+
+  Uint8List toUint8List() {
+    String savableValue = this.toString();
+    List<int> bytes = utf8.encode(savableValue);
+    return Uint8List.fromList(CryptoHelper().encrypt(bytes));
+  }
+
+  bool isEmptyItem() {
+    return name == "EMPTY" && password == "EMPTY";
   }
 
   @override
