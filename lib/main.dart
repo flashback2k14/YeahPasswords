f0725c41a45f8728bce257e4ad35ae2ff41290c7
flashback2k14 <yeahdev@gmail.com>
2020-04-13 00:02:47 +0200

    dev: implement search view

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,3 @@ import 'package:flutter/material.dart';
 import 'package:yeah_passwords/src/app.dart';
 
 void main() => runApp(App());
-
-// TODO:
-//  - add search view: https://stackoverflow.com/questions/49966980/how-to-create-toolbar-searchview-in-flutter
