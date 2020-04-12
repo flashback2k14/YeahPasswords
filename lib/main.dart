c27c9e1171e0b2135ee1fe9ab5e2a83a6a1ba3a4
flashback2k14 <yeahdev@gmail.com>
2020-04-12 14:42:59 +0200

    dev: bump versions

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,3 @@ import 'package:flutter/material.dart';
 import 'package:yeah_passwords/src/app.dart';
 
 void main() => runApp(App());
-
-// https://bloclibrary.dev/#/recipesfluttershowsnackbar
-// https://medium.com/@azamsharp/understanding-global-state-in-flutter-using-redux-2017b7646574
-// https://medium.com/flutter-community/use-environment-variables-in-ci-cd-for-flutter-apps-cbd6d9ac82c
-// https://pusher.com/tutorials/local-data-flutter
-// https://materialdesignicons.com/
-
-// https://www.filledstacks.com/snippet/multi-fab-menu-in-flutter-with-unicorndial/
-
-// TODO:
-//  - improve snackbars
